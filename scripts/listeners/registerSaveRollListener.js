83c9144290f6d11ad6228199c8077a8237c09212
Illia Osipov <iosipov83@gmail.com>
2026-01-31 19:35:46 +0200

    save rolls fix

--- scripts/listeners/registerSaveRollListener.js
+++ scripts/listeners/registerSaveRollListener.js
@@ -1,11 +1,11 @@
 import { registerAction } from "../utils/registerAction.js";
-import { getSaveTarget } from "../utils/getSaveTarget.js";
 import { DW_ROLL_SAVE } from "../constants/templateAttributes.js";
 
 export function registerSaveRollListener(html, { actor, getDwFlags, rollTargetCheck, prettyKey }) {
     registerAction(html, DW_ROLL_SAVE, async (ev) => {
         const key = ev.currentTarget.dataset.key;
-        const target = getSaveTarget(getDwFlags, key);
+        const target = Number(foundry.utils.getProperty(getDwFlags(), `saves.${key}`) ?? 0);
+
         await rollTargetCheck(actor, `Save: ${prettyKey(key)}`, target);
     });
 }
