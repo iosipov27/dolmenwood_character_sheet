82673126039cce3c91e835f1b8aa2e510f684157
illia <iosipov83@gmail.com>
2026-02-01 11:02:28 +0200

    context to templates

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { DolmenwoodSheet } from "./sheets/dolmenwoodSheet.js";
 Hooks.once("init", async (): Promise<void> => {
   const templatePaths: Record<string, string> = {
     "player-data": "modules/dolmenwood/templates/parts/player-data.hbs",
-    "health-points": "modules/dolmenwood/templates/parts/health-points.hbs"
+    "health-points": "modules/dolmenwood/templates/parts/health-points.hbs",
+    "save-throws": "modules/dolmenwood/templates/parts/save-throws.hbs"
   };
 
   await loadTemplates(templatePaths);
