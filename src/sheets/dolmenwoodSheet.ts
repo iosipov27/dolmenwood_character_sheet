d27a31da2d2d06bbfe0dd3e87aa0380448c0d53b
YAKOV_S\iosip <iosipov83@gmail.com>
2026-02-23 16:32:40 +0200

    refactor: switch to defaultOptions tabs

--- src/sheets/dolmenwoodSheet.ts
+++ src/sheets/dolmenwoodSheet.ts
@@ -1,29 +1,28 @@
 import { MODULE_ID } from "../constants/moduleId.js";
-import { TabController } from "../controllers/tabController.js";
 import { FormDataHandler } from "../handlers/formDataHandler.js";
 import { registerSheetListeners } from "../listeners/registerSheetListeners.js";
-import { registerTabNavigationListener } from "../listeners/registerTabNavigationListener.js";
 import { DolmenwoodSheetData } from "../models/dolmenwoodSheetData.js";
 import { DwFlagsRepository } from "../repositories/dwFlagsRepository.js";
 import type { DwSheetData, HtmlRoot } from "../types.js";
 import { getBaseOSECharacterSheetClass } from "../utils/getBaseOSECharacterSheetClass.js";
 
 const BaseSheet = getBaseOSECharacterSheetClass() as typeof foundry.appv1.sheets.ActorSheet;
+const TAB_GROUP = "dolmenwood-sheet-tabs";
+const TAB_NAV_SELECTOR = ".dolmenwood-sheet__tabs";
+const TAB_CONTENT_SELECTOR = ".dolmenwood-sheet__content";
 
 export class DolmenwoodSheet extends BaseSheet {
-  private readonly tabController: TabController;
   private readonly flagsRepository: DwFlagsRepository;
   private readonly formDataHandler: FormDataHandler;
 
   constructor(...args: ConstructorParameters<typeof BaseSheet>) {
     super(...args);
-    this.tabController = new TabController();
     this.flagsRepository = new DwFlagsRepository(this.actor);
     this.formDataHandler = new FormDataHandler(this.flagsRepository, this.actor);
   }
 
   static get defaultOptions(): ActorSheet.Options {
-    return foundry.utils.mergeObject(super.defaultOptions, {
+    const options = foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["dolmenwood", "sheet", "actor"],
       template: `modules/${MODULE_ID}/templates/dolmenwood.hbs`,
       width: 640,
@@ -33,6 +32,17 @@ export class DolmenwoodSheet extends BaseSheet {
       submitOnChange: true,
       resizable: true
     });
+
+    options.tabs = [
+      {
+        group: TAB_GROUP,
+        navSelector: TAB_NAV_SELECTOR,
+        contentSelector: TAB_CONTENT_SELECTOR,
+        initial: "main"
+      }
+    ];
+
+    return options;
   }
 
   getData(options?: Partial<ActorSheet.Options>): DwSheetData {
@@ -43,11 +53,6 @@ export class DolmenwoodSheet extends BaseSheet {
 
   activateListeners(html: HtmlRoot): void {
     super.activateListeners(html);
-
-    registerTabNavigationListener(html, {
-      getActiveTab: () => this.tabController.getActiveTab(),
-      setActiveTab: (tab: string) => this.tabController.setActiveTab(tab)
-    });
 
     registerSheetListeners(html, {
       actor: this.actor,
